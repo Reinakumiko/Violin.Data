aa666567e8081b8100077f623a9a4b9e814640f6
_Kumiko <1164114736@qq.com>
2017-08-08 14:22:47 +0800

    Update attribute field & string format support

--- Violin.Data/Database/Attributes/NameAttribute.cs
+++ Violin.Data/Database/Attributes/NameAttribute.cs
@@ -8,13 +8,23 @@ namespace Violin.Data.Database.Attributes
 	/// <summary>
 	/// 用于标记类或成员在数据库中的名称
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class|AttributeTargets.Struct|AttributeTargets.Method|AttributeTargets.Property, Inherited = true, AllowMultiple = true)]
+	[AttributeUsage(AttributeTargets.Class|AttributeTargets.Struct|AttributeTargets.Method|AttributeTargets.Property|AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
 	public sealed class NameAttribute : Attribute
 	{
 		/// <summary>
 		/// 成员名称
 		/// </summary>
 		public string Name { get; set; }
+
+		/// <summary>
+		/// 为字段指定数据类型
+		/// </summary>
+		public Type Type { get; set; }
+
+		/// <summary>
+		/// 为字段设置数据格式
+		/// </summary>
+		public string Format { get; set; }
 
 		/// <summary>
 		/// 实例化一个新的特性以标记该成员的名称
--- Violin.Data/Reflection/ReflectionExtension.cs
+++ Violin.Data/Reflection/ReflectionExtension.cs
@@ -38,8 +38,20 @@
 				//获取字段值
 				dynamic value = property.GetValue(entity, null);
 
+				//将数据转换为指定类型
+				if (attrColumn.Type != null)
+					value = Convert.ChangeType(value, attrColumn.Type);
+
+				//将数据以指定格式序列化
+				if (!string.IsNullOrWhiteSpace(attrColumn.Format))
+				{
+					var formatString = string.Format("{{0:{0}}}", attrColumn.Format);
+
+					value = string.Format(formatString, value);
+				}
+				
 				//将时间转换为标准格式
-				if (value is DateTime)
+				if (value is DateTime && string.IsNullOrWhiteSpace(attrColumn.Format))
 					value = value.ToString("yyyy-MM-dd HH:mm:ss");
 
 				//将特性的列名与字段值添加到键值对
